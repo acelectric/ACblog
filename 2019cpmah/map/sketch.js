a70f83c057a8e309e1222b058bfe760702b78f72
acelectric <ianlu0415@gmail.com>
2019-08-07 18:46:36 +0800

    map

--- 2019cpmah/map/sketch.js
+++ 2019cpmah/map/sketch.js
@@ -61,15 +61,27 @@ function setup() {
                 areas[i].eName = eName[j][1];
             }
         }
-        areas[i].reSize();
+        //areas[i].reSize();
     }
+    for (let i = 0; i < areas.length; i++) {
+        for (let j = 0; j < areas[i].path.length; j++) {
+            for (let k = 0; k < areas[i].path[j].length; k++) {
+                for (let l = 0; l < areas[i].path[j][k].length; l++) {
+                    areas[i].path[j][k][l][1] += 55;
+                }
+            }
+        }
+    }
+    let str = JSON.stringify(areas, ["feature", "eName", "cName", "path"]);
 
-
+    console.log(str);
     console.log(areas);
 
     for (let i = 0; i < areas.length; i++) {
         createA(areas[i].eName + ".html", "").id(areas[i].eName);
     }
+
+
 
     //-------------
     // noLoop();
